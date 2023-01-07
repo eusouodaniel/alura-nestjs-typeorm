2fc2031762640b92a4e247f16d8befa6830970eb
eusouodaniel <danielrodriguesdrs331@gmail.com>
2023-01-06 22:11:37 -0300

    new: lesson 3.2

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,6 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class UsuarioEntity {
+  @PrimaryGeneratedColumn()
   id: string;
+  @Column({ length: 100 })
   nome: string;
+  @Column({ length: 100 })
   email: string;
+  @Column({ length: 100 })
   senha: string;
 }
